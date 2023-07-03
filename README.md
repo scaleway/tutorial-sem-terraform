eee66e4b3c4014564841f86e724a5fbd6cd87ee1
jaime Bernabe <6184069+Monitob@users.noreply.github.com>
2023-07-03 15:34:28 +0200

    docs: add tutorial sem and demo-app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tutorial-sem-terraform
+
+This repository contains materials (demo-app, infrastructures and other code examples) used for tutorial, training
+sessions around the theme of Secret Manager ([SEM](https://www.scaleway.com/fr/secret-manager/)).
+
+These materials have been designed around the following principles:
+
+they assume very little prior knowledge of Terraform, Scaleway tools(SDK, CLI) or a particular programming language;
+they are hands-on, meaning that they contain lots of examples and exercises that you can easily reproduce;
---
+++ examples/key-value/app/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/scaleway/scaleway-sdk-go/api/rdb/v1"
+	secret "github.com/scaleway/scaleway-sdk-go/api/secret/v1alpha1"
+	"github.com/scaleway/scaleway-sdk-go/scw"
+)
+
+const (
+	dbname           = "rdb"
+	databaseInstance = "dataBaseInstance"
+)
+
+// Auth credentials
+type Auth struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+
+	organizationID := os.Getenv("SCW_DEFAULT_ORGANIZATION_ID")
+	accessKey := os.Getenv("SCW_ACCESS_KEY")
+	secretKey := os.Getenv("SCW_SECRET_KEY")
+	defaultRegion := os.Getenv("SCW_DEFAULT_REGION")
+	fmt.Printf("DEFAULT REGION: %s", defaultRegion)
+	// Create a Scaleway client
+	client, err := scw.NewClient(
+		// Get your organization ID at https://console.scaleway.com/organization/settings
+		scw.WithDefaultOrganizationID(organizationID),
+		scw.WithDefaultRegion(scw.Region(defaultRegion)),
+		// Get your credentials at https://console.scaleway.com/iam/api-keys
+		scw.WithAuth(accessKey, secretKey),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create SDK objects for Scaleway Secret Manager product
+	secretApi := secret.NewAPI(client)
+
+	// Name of the secret
+	secretName := "database_secret"
+	// Call the GetSecretVersionByName method on the Secret SDK
+	response, err := secretApi.AccessSecretVersionByName(&secret.AccessSecretVersionByNameRequest{
+		SecretName: secretName,
+		Revision:   "latest",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal the credentials
+	var auth Auth
+	err = json.Unmarshal(response.Data, &auth)
+	if err != nil {
+		panic(err)
+	}
+
+	rdbAPI := rdb.NewAPI(client)
+	instanceName := databaseInstance
+	rdbInstance, err := rdbAPI.ListInstances(&rdb.ListInstancesRequest{Name: &instanceName})
+	if err != nil {
+		panic(err)
+	}
+
+	i := rdbInstance.Instances[0]
+	// Retrieve the database endpoint
+	var endpointData *rdb.Endpoint
+	for _, endpoint := range i.Endpoints {
+		if endpoint.LoadBalancer != nil {
+			endpointData = endpoint
+			break
+		}
+	}
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		endpointData.IP, endpointData.Port, auth.Username, auth.Password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
